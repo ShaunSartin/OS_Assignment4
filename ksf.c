0705ecaf73acda776809fff957c033a73e0ea41c
ShaunSartin <luckydice2323@yahoo.com>
2017-04-18 17:16:01 -0500

    Kerbals can perform basic actions
    I'm not convinced that this code is entirely correct.
    I believe that I need at least one mutex or a semaphore in order to ensure proper behavior.
    
    The basic idea is that now all the parts are global variables, so that each thread can share this data.
    When a thread assembles a machine, it grabs one of each part and decrements its current value.
    It then goes to sleep and disassembles the machine, incrementing the values.
    This process repeats endlessly.

--- ksf.c
+++ ksf.c
@@ -3,6 +3,21 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <semaphore.h>
+
+
+// A user-inputted value specifying the total number of engines
+int engineNum;
+
+// A user-inputted value specifying the total number of fuselages	
+int fuselageNum;
+	
+// A user-inputted value specifying the total number of fuel tanks
+int fuelTankNum;
+	
+// A user-inputted value specifying the total number of Kerbals
+int kerbalNum;
+
 
 /* 
 Function Purpose: 
@@ -22,13 +37,34 @@ Function Purpose:
 NOTE: 
 	Maybe we need the infinite loop to be in main?
 	Shouldn't this cause an infinite number of each thread to be created?
+	printf() causing them all to be dumped together? Or are they actuallly being synchronized because of pthread_join()?
 */
-void* test(void* kerbalID)
+void* begin_sequence(void* kerbalID)
 {
 	while(1)
 	{
-		printf("Hello from Kerbal #%ld\n", (long) kerbalID);
-		sleep(2);
+		printf("Kerbal %ld: Enters into the assembly facility\n", (long) kerbalID);
+		
+		//Mutex or Semaphore needed here?
+		//What if another thread steals the last available resource after another thread confirms if-statement?
+		if((engineNum > 0) && (fuselageNum > 0) && (fuelTankNum > 0))
+		{
+			engineNum -= 1;
+			fuselageNum -= 1;
+			fuelTankNum -= 1;
+			printf("Kerbal %ld: Assembled Engine (%d Engines, %d Fuselages, and %d Fuel Tanks remaining)\n",
+				 (long) kerbalID, engineNum, fuselageNum, fuelTankNum);
+			
+			sleep(5);	//NOTE: ASSEMBLY TAKES 15 SECONDS	
+			
+			engineNum += 1;
+			fuselageNum += 1;
+			fuelTankNum += 1;
+			printf("Kerbal %ld: Disassembled Engine (%d Engines, %d Fuselages, and %d Fuel Tanks remaining)\n",
+				 (long) kerbalID, engineNum, fuselageNum, fuelTankNum);
+			
+		}
+		sleep(5);
 	}
 }
 
@@ -41,17 +77,11 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	// A user-inputted value specifying the total number of engines
-	int engineNum = atoi(argv[1]);
-
-	// A user-inputted value specifying the total number of fuselages
-	int fuselageNum = atoi(argv[2]);
-	
-	// A user-inputted value specifying the total number of fuel tanks
-	int fuelTankNum = atoi(argv[3]);
-	
-	// A user-inputted value specifying the total number of Kerbals
-	int kerbalNum = atoi(argv[4]);
+	// Assign the user-inputted values to their respective global variable
+	engineNum = atoi(argv[1]);
+	fuselageNum = atoi(argv[2]);
+	fuelTankNum = atoi(argv[3]);
+	kerbalNum = atoi(argv[4]);
 
 	// A counter used in `for-loops`
 	long i;
@@ -71,9 +101,10 @@ int main(int argc, char* argv[])
 
 	for (i = 0; i < kerbalNum; i++)
 	{	
-		pthread_create(&kerbalArray[i], NULL, test, (void*) i);
+		pthread_create(&kerbalArray[i], NULL, begin_sequence, (void*) i);
 	}
 
+	//	pthread_join(kerbalArray[0], NULL);
 	// NOTE: For some reason, we need this 'join' otherwise there is no output.
 	// As far as I understand it, we shouldn't need join.		
 	for(i = 0; i < kerbalNum; i++)
