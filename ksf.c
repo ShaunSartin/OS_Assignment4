573caab50f692fdeed6f833d5ed1d5afb7d4dc5a
ShaunSartin <luckydice2323@yahoo.com>
2017-04-18 02:55:50 -0500

    Simple thread implementation
    Kerbals introduce infinitely themselves.
    However, they all seem to wait for all other kerbals to finish introducing themselves before giving another introduction.
    ^ I assume this is because of pthread_join().

--- ksf.c
+++ ksf.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
-//Function Purpose: To display a message to the user if they pass invalid values as parameters when initally executing this program.
+/* 
+Function Purpose: 
+	To display a message to the user if they pass invalid values as parameters when initally executing this program.
+*/
 void print_input_error()
 {
 	printf("An input-based error occurred. Be sure to enter four integers between 1 and 255.\n");
@@ -11,6 +15,22 @@ void print_input_error()
 	printf("./[compiled_name] 1 2 3 4\n");
 }
 
+
+/*
+Function Purpose:
+	Test a particular kerbal (pthread) value.
+NOTE: 
+	Maybe we need the infinite loop to be in main?
+	Shouldn't this cause an infinite number of each thread to be created?
+*/
+void* test(void* kerbalID)
+{
+	while(1)
+	{
+		printf("Hello from Kerbal #%ld\n", (long) kerbalID);
+		sleep(2);
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -33,17 +53,37 @@ int main(int argc, char* argv[])
 	// A user-inputted value specifying the total number of Kerbals
 	int kerbalNum = atoi(argv[4]);
 
+	// A counter used in `for-loops`
+	long i;
+
 	// Check to see if acceptable values were passed to main()
-	if( !((engineNum > 0) && (engineNum < 255) 
-		&& (fuselageNum > 0) && (fuselageNum < 255)
-		&& (fuelTankNum > 0) && (fuelTankNum < 255)
-		&& (kerbalNum > 0) && (kerbalNum < 255)) )
+	if( !((engineNum > 0) && (engineNum < 256) 
+		&& (fuselageNum > 0) && (fuselageNum < 256)
+		&& (fuelTankNum > 0) && (fuelTankNum < 256)
+		&& (kerbalNum > 0) && (kerbalNum < 256)) )
 	{
 		print_input_error();
 		return -1;
 	}
 
-	printf("%d, %d, %d, %d\n", engineNum, fuselageNum, fuelTankNum, kerbalNum);
+	// An array will be used to hold all of the kerbals (pthreads)
+	pthread_t kerbalArray [kerbalNum];
+
+	for (i = 0; i < kerbalNum; i++)
+	{	
+		pthread_create(&kerbalArray[i], NULL, test, (void*) i);
+	}
+
+	// NOTE: For some reason, we need this 'join' otherwise there is no output.
+	// As far as I understand it, we shouldn't need join.		
+	for(i = 0; i < kerbalNum; i++)
+	{
+		if(pthread_join(kerbalArray[i], NULL) != 0)
+		{
+			printf("DEADLOCK DETECTED.\n");
+			return -1;
+		}
+	}
 
 	return 0;
 }
